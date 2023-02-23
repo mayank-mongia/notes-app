4e99834f713aa2595d1a7187b5e9ab3eaac6caf0
Mayank Mongia <mongia.mayank@gmail.com>
2023-02-23 10:54:35 +0530

    Updates

---
+++ precache-manifest.08197c9166bb27ade1ab9313a25bdae7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c624e56436f220f90aee3cd6e8f48f0",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "1637e224d81a1f5a8b49",
+    "url": "/Notes-App/static/js/2.ca38bd25.chunk.js"
+  },
+  {
+    "revision": "5e6f47aca51ec4452c4a",
+    "url": "/Notes-App/static/js/main.d9679386.chunk.js"
+  },
+  {
+    "revision": "a8020ccc5240e5752df1",
+    "url": "/Notes-App/static/js/runtime-main.759b2abb.js"
+  }
+]);
